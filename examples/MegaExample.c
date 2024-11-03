a73e1a0dfe16eb9f26b03cd77c37622eb985e74e
SabeDoesThings <122580233+SabeDoesThings@users.noreply.github.com>
2024-11-03 11:24:18 -0600

    Add files via upload

--- examples/MegaExample.c
+++ examples/MegaExample.c
@@ -6,18 +6,18 @@ const int WINDOW_WIDTH = 1280;
 const int WINDOW_HEIGHT = 720;
 
 typedef struct {
-	nxTexture2D tex;
-	int x;
-	int y;
+    nxTexture2D tex;
+    int x;
+    int y;
 } Square;
 
 Square square;
 nxAnimation* anim;
 
 void on_run() {
-	nx_play_music_looped("./examples/res/music.mp3");
+    nx_play_music_looped("./examples/res/music.mp3");
 
-	square.tex = nx_load_texture("./examples/res/newnexa.png");
+    square.tex = nx_load_texture("./examples/res/newnexa.png");
     square.x = WINDOW_WIDTH / 2 - square.tex.width / 2;
     square.y = WINDOW_HEIGHT / 2 - square.tex.height / 2;
 
@@ -25,55 +25,54 @@ void on_run() {
 }
 
 void update(float dt) {
-	if (nx_is_key_down("w")) {
-		square.y -= 400 * dt;
-	}
-	if (nx_is_key_down("a")) {
-		square.x -= 400 * dt;
-	}
-	if (nx_is_key_down("s")) {
-		square.y += 400 * dt;
-	}
-	if (nx_is_key_down("d")) {
-		square.x += 400 * dt;
-	}
+    if (nx_is_key_down(nxKEY_W)) {
+        square.y -= 400 * dt;
+    }
+    if (nx_is_key_down(nxKEY_A)) {
+        square.x -= 400 * dt;
+    }
+    if (nx_is_key_down(nxKEY_S)) {
+        square.y += 400 * dt;
+    }
+    if (nx_is_key_down(nxKEY_D)) {
+        square.x += 400 * dt;
+    }
 
-	if (nx_is_key_pressed("p")) {
-		nx_play_audio("./examples/res/sfx.wav");
-	}
+    if (nx_is_key_pressed(nxKEY_P)) {
+        nx_play_audio("./examples/res/sfx.wav");
+    }
 
-	if (nx_is_mouse_button_pressed("left")) {
-		printf("left mouse button pressed\n");
-	}
-	if (nx_is_mouse_button_down("right")) {
-		printf("right mouse button down\n");
-	}
+    if (nx_is_mouse_button_pressed(nxMOUSE_LEFT)) {
+        printf("left mouse button pressed\n");
+    }
+    if (nx_is_mouse_button_down(nxMOUSE_RIGHT)) {
+        printf("right mouse button down\n");
+    }
 
-	nx_update_animation(anim, dt, true);
+    nx_update_animation(anim, dt, true);
 
-	int x, y;
-	nx_get_mouse_position(&x, &y);
-	printf("x: %d, y: %d\n", x, y);
+    int x, y;
+    nx_get_mouse_position(&x, &y);
+    printf("x: %d, y: %d\n", x, y);
 }
 
 void render(nxContext* ctx) {
-	nx_clear_screen(ctx, nxCORNFLOWERBLUE);
+    nx_clear_screen(ctx, nxCORNFLOWERBLUE);
 
-	nx_render_texture(ctx, &square.tex, square.x, square.y, 1.0f, 1.0f);
+    nx_render_texture(ctx, &square.tex, square.x, square.y, 1.0f, 1.0f, 0.0f);
 
-	nx_render_rect_line(ctx, 300, 60, 64, 64, nxRED);
-	nx_render_rect_filled(ctx, 300, 400, 64, 64, nxWHITE);
+    nx_render_rect_line(ctx, 300, 60, 64, 64, nxRED);
+    nx_render_rect_filled(ctx, 300, 400, 64, 64, nxWHITE);
 
-	nx_render_circle_line(ctx, 320, 240, 100, nxGREEN);
-	nx_render_circle_filled(ctx, 320, 600, 100, nxYELLOW);
+    nx_render_circle_line(ctx, 320, 240, 100, nxGREEN);
+    nx_render_circle_filled(ctx, 320, 600, 100, nxYELLOW);
 
-	nx_render_text(ctx, nx_load_font("./examples/res/arial.ttf", 50), "Hello World!", nxBLACK, 0, 0, 2.0f, 6.0f);
+    nx_render_text(ctx, nx_load_font("./examples/res/arial.ttf", 50), "Hello World!", nxBLACK, 0, 0, 2.0f, 6.0f);
 
-	nx_render_animation(ctx, anim, 100, 100, 5.0f, 3.0f);
+    nx_render_animation(ctx, anim, 100, 100, 5.0f, 3.0f, 1.0f);
 }
 
 int main(int argc, char *argv[]) {
-	nx_start(on_run, update, render, "My Window", WINDOW_WIDTH, WINDOW_HEIGHT, false);
-
-	return 0;
+    nx_start(on_run, update, render, "My Window", WINDOW_WIDTH, WINDOW_HEIGHT, false);
+    return 0;
 }
